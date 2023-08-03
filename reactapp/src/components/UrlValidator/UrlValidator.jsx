4b64da68af79a3f92b89116a780900585107e600
Iamneo <techadmin@iamneo.ai>
2023-08-03 09:46:49 +0000

    Backup Commit

--- reactapp/src/components/UrlValidator/UrlValidator.jsx
+++ reactapp/src/components/UrlValidator/UrlValidator.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from 'react';
-
+ 
 const UrlValidator = () => {
-
+ 
     let [disabled,setDisabled] = useState(false);
-
+ 
     const isValidateDomain = (d) => {
         if(d.startsWith("www.") && d.endsWith(".com")) {
             return true;
         }
         return false;
     }
-
+ 
     const convertToPath = (input) => {
         return '/' + input.split(" ").join("/");
     }
-
+ 
     const convertJson = (jsonString) => {
         const jsonObject = JSON.parse(jsonString);
         const params = "?" + Object.entries(jsonObject).map((e) => e.join("=")).join("&");
         return params;
     }
-
+ 
     const isValidJson = (jsonString) => {
         try {
             JSON.parse(jsonString);
@@ -29,7 +29,7 @@ const UrlValidator = () => {
             return false;
         }
     }
-
+ 
     const validateForm = (domain,path,method,body) => {
         if (!isValidateDomain(domain)) {
             console.log("went");
@@ -48,7 +48,7 @@ const UrlValidator = () => {
         }
         return "";
     }
-
+ 
     const handleSubmit = async (e) => {
         e.preventDefault();
         let domain = e.target[0].value;
@@ -68,13 +68,13 @@ const UrlValidator = () => {
         else {
             document.getElementById("message").textContent = tmp_msg;
         }
-
+ 
     }
     return (
         <div className="main-div">
             <div data-testid="message" id="message"></div>
             <form data-testid="submit" onSubmit={handleSubmit}>
-
+ 
                 <label>
                     Domain:
                     <input data-testid="domain" type="text" />
@@ -110,179 +110,5 @@ const UrlValidator = () => {
         </div>
     );
 }
-
+ 
 export default UrlValidator;
-urlvalidatortest.js
-import UrlValidator from "../components/UrlValidator/UrlValidator";
-import { render, screen, cleanup, fireEvent } from '@testing-library/react';
-
-describe('Test URL Validator', () => {
-
-    test('testcase1', () => {
-        render(<UrlValidator />);
-
-        const inputDomain = screen.getByTestId('domain');
-        const inputPath = screen.getByTestId('path');
-        const inputMethod = screen.getByTestId('method');
-        const inputBody = screen.getByTestId('body');
-
-        const form = screen.getByTestId('submit');
-
-        expect(inputDomain).toBeTruthy();
-        expect(inputPath).toBeTruthy();
-        expect(inputMethod).toBeTruthy();
-        expect(inputBody).toBeTruthy();
-
-        
-        fireEvent.submit(form, {
-            target : [
-                {value : 'www.google.com'},
-                {value : 'search all'},
-                {value : 'GET'},
-                {value : ''},
-                
-            ]
-        });
-
-        const message = screen.getByTestId('message');
-        expect(message.textContent).toBe('www.google.com/search/all');
-
-    });
-
-    test('testcase2', () => {
-        render(<UrlValidator />);
-
-        const inputDomain = screen.getByTestId('domain');
-        const inputPath = screen.getByTestId('path');
-        const inputMethod = screen.getByTestId('method');
-        const inputBody = screen.getByTestId('body');
-
-        const form = screen.getByTestId('submit');
-
-        expect(inputDomain).toBeTruthy();
-        expect(inputPath).toBeTruthy();
-        expect(inputMethod).toBeTruthy();
-        expect(inputBody).toBeTruthy();
-
-        
-        fireEvent.submit(form, {
-            target : [
-                {value : 'www.google.com'},
-                {value : 'search all'},
-                {value : 'GET'},
-                {value : '{\"Name\":\"Max\"}'},
-                
-            ]
-        });
-
-        const message = screen.getByTestId('message');
-        expect(message.textContent).toBe('www.google.com/search/all?Name=Max');
-
-    });
-
-    test('testcase3', () => {
-        render(<UrlValidator />);
-
-        const form = screen.getByTestId('submit');
-        const inputMethod = screen.getByTestId('method');
-
-        fireEvent.change(inputMethod, {target : {value : 'POST'}})
-        
-        fireEvent.submit(form, {
-            target : [
-                {value : 'www.google.com'},
-                {value : 'posts'},
-                {value : 'POST'},
-                {value : '{\"Name\":\"Max\"}'}
-            ]
-        });
-
-        const message = screen.getByTestId('message');
-        expect(message.textContent).toBe('www.google.com/posts');
-
-    })
-
-    test('testcase4', () => {
-        render(<UrlValidator />);
-
-        const inputDomain = screen.getByTestId('domain');
-        const inputPath = screen.getByTestId('path');
-        const inputMethod = screen.getByTestId('method');
-        const body = screen.getByTestId('body');
-
-        const form = screen.getByTestId('submit');
-        
-        fireEvent.change(inputDomain, {target : {value: 'www.google.com'}});
-        fireEvent.change(inputPath, {target : {value: 'search all'}});
-        fireEvent.change(inputMethod, {target : {value: 'POST'}});
-        
-        fireEvent.submit(form, {
-            target : [
-                {value : 'www.google.com'},
-                {value : 'search all'},
-                {value : 'POST'},
-                {value : ''},
-                
-            ]
-        });
-
-        const message = screen.getByTestId('message');
-        expect(message.textContent).toBe('Error in the Body');
-
-    })
-
-    test('testcase5', () => {
-        render(<UrlValidator />);
-
-        const form = screen.getByTestId('submit');
-        const inputMethod = screen.getByTestId('method');
-
-        fireEvent.change(inputMethod, {target : {value : 'DELETE'}})
-        
-        fireEvent.submit(form, {
-            target : [
-                {value : 'www.google.com'},
-                {value : 'posts'},
-                {value : 'DLETE'},
-                {value : ''}
-            ]
-        });
-
-        const message = screen.getByTestId('message');
-        expect(message.textContent).toBe('www.google.com/posts');
-
-    });
-
-    test('testcase6', () => {
-        render(<UrlValidator />);
-
-        const inputDomain = screen.getByTestId('domain');
-        const inputPath = screen.getByTestId('path');
-        const inputMethod = screen.getByTestId('method');
-        const inputBody = screen.getByTestId('body');
-
-        const form = screen.getByTestId('submit');
-
-        expect(inputDomain).toBeTruthy();
-        expect(inputPath).toBeTruthy();
-        expect(inputMethod).toBeTruthy();
-        expect(inputBody).toBeTruthy();
-
-        
-        fireEvent.submit(form, {
-            target : [
-                {value : 'www.google.com'},
-                {value : 'search all'},
-                {value : 'GET'},
-                {value : '{\"Name\":\"Max\"'},
-                
-            ]
-        });
-
-        const message = screen.getByTestId('message');
-        expect(message.textContent).toBe('Error in the Body of the Query Params');
-
-    });
-
-})
-
